e377c3a22f9cb5c11604dd64681da1c31c8b5f60
JanuszPXYZ <19962689+JanuszPXYZ@users.noreply.github.com>
2020-05-13 21:15:03 +0200

    Initial Commit

---
+++ Project 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 5
+//
+//  Created by Janusz Polowczyk on 13/05/2020.
+//  Copyright © 2020 Janusz Polowczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
